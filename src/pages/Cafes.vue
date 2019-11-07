9b1b78be574d1f8e40d9934ece3da74cee9c978d
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-11-07 08:00:10 -0500

    Move cafe model mapper to helper

--- src/pages/Cafes.vue
+++ src/pages/Cafes.vue
@@ -70,6 +70,8 @@ query Cafes {
 </static-query>
 
 <script>
+import { mapCafe } from '../helpers/cafeHelpers'
+
 export default {
   computed: {
     cafes: function () {
@@ -97,24 +99,6 @@ export default {
     getLocationCafes: function (location) {
       return this.cafes.filter(c => c.location === location)
     }
-  }
-}
-
-function mapCafe (cafe) {
-  const countryWithState = cafe.country + (cafe.state ? `, ${cafe.state}` : '')
-  const location = `${cafe.city}, ${countryWithState}`
-  return {
-    name: cafe.name,
-    photo: cafe.photo[0] ? cafe.photo[0].url : null,
-    street: cafe.street,
-    city: cafe.city,
-    zipCode: cafe.zipCode,
-    country: cafe.country,
-    state: cafe.state,
-    phone: cafe.phone,
-    dataAddress: `${cafe.city}, ${cafe.street}`,
-    countryWithState,
-    location,
   }
 }
 </script>
